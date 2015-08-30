10c204d760c358b06d7d345af08cd96279e18f3f
shu223 <shuichi0526@gmail.com>
2015-08-30 23:09:32 +0200

    Improved Text Detector sample as use `subFeatures` property

--- iOS9Sampler/SampleViewControllers/TextDetectViewController.swift
+++ iOS9Sampler/SampleViewControllers/TextDetectViewController.swift
@@ -21,9 +21,6 @@ class TextDetectViewController: UIViewController {
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
-        self.detect(imageView1)
-        self.detect(imageView2)
     }
     
     override func didReceiveMemoryWarning() {
@@ -42,6 +39,15 @@ class TextDetectViewController: UIViewController {
     }
     */
     
+    private func rectForFeature(feature: CITextFeature, bounds: CGRect, scale: CGFloat) -> CGRect {
+        
+        return CGRectMake(
+            feature.topLeft.x * scale,
+            bounds.height - feature.topLeft.y * scale,
+            feature.bounds.size.width * scale,
+            feature.bounds.size.height * scale)
+    }
+    
     private func detect(imageView: UIImageView) {
         
         let image  = CIImage(CGImage: imageView.image!.CGImage!)
@@ -51,22 +57,45 @@ class TextDetectViewController: UIViewController {
             context: nil,
             options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])
         
-        let features = detector.featuresInImage(image)
+        let features = detector.featuresInImage(
+            image,
+            options: [CIDetectorReturnSubFeatures: NSNumber(bool: true)])
         let scale = imageView.frame.size.width / image.extent.width
         
         for feature in features as! [CITextFeature] {
             print("bounds:\(feature.bounds), topLeft:\(feature.topLeft), bottomRight:\(feature.bottomRight)")
             
-            let featureRect = CGRectMake(
-                feature.topLeft.x * scale,
-                imageView.bounds.height - feature.topLeft.y * scale,
-                feature.bounds.size.width * scale,
-                feature.bounds.size.height * scale)
+            // draw feature rects
+            let featureRect = self.rectForFeature(feature, bounds: imageView.bounds, scale: scale)
             let featureView = UIView(frame: featureRect)
             featureView.backgroundColor = UIColor.clearColor()
             featureView.layer.borderColor = UIColor.greenColor().colorWithAlphaComponent(0.8).CGColor
             featureView.layer.borderWidth = 2.0
             imageView.addSubview(featureView)
+            
+            // draw subFeature rects
+            for item in feature.subFeatures {
+
+                let subFeature = item as! CITextFeature
+                let subFeatureRect = self.rectForFeature(subFeature, bounds: imageView.bounds, scale: scale)
+                let subFeatureView = UIView(frame: subFeatureRect)
+                subFeatureView.backgroundColor = UIColor.clearColor()
+                subFeatureView.layer.borderColor = UIColor.yellowColor().colorWithAlphaComponent(0.8).CGColor
+                subFeatureView.layer.borderWidth = 1.0
+                imageView.addSubview(subFeatureView)
+            }
         }
+    }
+    
+    
+    // =========================================================================
+    // MARK: - Actions
+    
+    @IBAction func detactBtnTapped(sender: UIButton) {
+
+        sender.hidden = true
+        
+        self.detect(imageView1)
+        self.detect(imageView2)
     }
 }
